e9f4215cde03ca7fc7f96d6ebe8646cd1fcd4476
Leone2606 <85848820+Leone2606@users.noreply.github.com>
2021-09-25 11:53:29 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import this
+
+#operadores :  + - / *
+
+print(2**4) #potência
+print(10 % 3) #resto
+print(10/3)
+print(10//3) #pega só a parte inteira do resultado
+
+#help()
+
+
+nome = "Murilo Leonel"
+print(nome[:5])
+print(len(nome))
+print(nome.lower())
+print(nome.upper())
+print(nome.capitalize())
+print("M" in nome)
+print(nome.count("e"))
+print(nome.startswith("M"))
+print (nome.endswith("l"))
